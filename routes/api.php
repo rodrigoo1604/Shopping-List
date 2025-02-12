ece29d03b8c703d943817ad26dbe0b215d6be2fb
rodrigoo1604 <rodrigo.lastra.pescador@gmail.com>
2025-02-12 10:48:27 +0100

    feat: added api routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Models\Article;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ArticleController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/articles', [ArticleController::class, 'index'])->name('apihome');
+Route::delete('/articles/destroy/{id}', [ArticleController::class, 'destroy'])->name('apidestroy');
+Route::post('/articles/store', [ArticleController::class, 'store'])->name('apistore');
+Route::put('/articles/update/{id}', [ArticleController::class, 'update'])->name('apiupdate');
+Route::get('/articles/truncate',[ArticleController::class, 'truncate'])->name('apitruncate');
